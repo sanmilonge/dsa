50a962d027eaa54ca5adc7bc7d1c0425a3602d0f
sanmilonge <139220972+sanmilonge@users.noreply.github.com>
2026-02-20 19:38:52 +0000

    Initial commit

---
+++ lab2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(){
+	int noOfy1Modules = 3;
+	int year_1grades[3] = {97, 93, 90};
+	
+	printf("Year 1 grades (%d modules): \n", noOfy1Modules);
+	for (int i = 0; i < noOfy1Modules; i++){
+		printf("Grade %d = %d\n", i + 1,  year_1grades[i]);
+	}
+	
+	int year_2grades[6];
+	for (int i = 0; i < noOfy1Modules; i++){
+		year_2grades[i] = year_1grades[i];
+		printf("Grade %d added to second array\n", i + 1);
+	}
+	year_2grades[noOfy1Modules] = 100;
+	year_2grades[noOfy1Modules + 1] = 100;
+	year_2grades[noOfy1Modules + 2] = 100;
+	int noOfy2Modules = noOfy1Modules + 3;
+	
+	printf("Year 2 grades (%d modules): \n", noOfy2Modules);
+	for (int i = 0; i < noOfy2Modules; i++){
+		printf("Grade %d = %d\n", i + 1,  year_2grades[i]);
+	} 
+	
+	printf("Size of year_1grades: %zu bytes\n", sizeof(year_1grades));
+	printf("Size of year_2grades: %zu bytes", sizeof(year_2grades));
+	
+}
